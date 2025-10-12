9c3cb3a40eb76f93b33f0ab8c84f6637575eb65f
Netetra <netetra07@gmail.com>
2025-10-12 15:55:12 +0900

    feat: コマンドの定義

--- src/nrcc-2025.ts
+++ src/nrcc-2025.ts
@@ -1,7 +1,23 @@
 import { Transform } from "node:stream";
 import type { TransformOptions, TransformCallback } from "node:stream";
 
-export type Commands = ReceiveSuccess | ReceiveFailed | Pong | CurrentLocation;
+export type Commands =
+  | ReceiveSuccess
+  | ReceiveFailed
+  | Ping
+  | Pong
+  | SetLocation
+  | CurrentLocation
+  | ArmStop
+  | LeftArmMove
+  | RightArmMove
+  | LeftArmFoldUpper
+  | RightArmFoldUpper
+  | LeftArmFoldLower
+  | RightArmFoldLower
+  | ArmSuctionOnOff;
+
+export type BoxSize = "A" | "B" | "C" | "D" | "E";
 
 export type ReceiveSuccess = {
   command: "receive_success";
@@ -12,8 +28,19 @@ export type ReceiveFailed = {
   error_code: number;
 };
 
+export type Ping = {
+  command: "ping";
+};
+
 export type Pong = {
   command: "pong";
+};
+
+export type SetLocation = {
+  command: "set_location";
+  x: number;
+  y: number;
+  degree: number;
 };
 
 export type CurrentLocation = {
@@ -23,6 +50,40 @@ export type CurrentLocation = {
   degree: number;
 };
 
+export type ArmStop = {
+  command: "arm_stop";
+};
+
+export type LeftArmMove = {
+  command: "left_arm_move";
+  box: BoxSize;
+}
+
+export type RightArmMove = {
+  command: "right_arm_move";
+  box: BoxSize;
+}
+
+export type LeftArmFoldUpper = {
+  command: "left_arm_fold_upper"
+}
+
+export type RightArmFoldUpper = {
+  command: "right_arm_fold_upper"
+}
+
+export type LeftArmFoldLower = {
+  command: "left_arm_fold_lower"
+}
+
+export type RightArmFoldLower = {
+  command: "right_arm_fold_lower"
+}
+
+export type ArmSuctionOnOff = {
+  command: "arm_suction_on_off";
+  is_on: boolean;
+}
 
 export function parse_nrcc2025(chunk: Buffer): Commands | undefined {
   if (chunk.length === 0) {
@@ -49,5 +110,97 @@ export function parse_nrcc2025(chunk: Buffer): Commands | undefined {
         degree: chunk.readInt32BE(9) / 100
       };
   }
+  return undefined;
+}
+
+export function build_nrcc2025(cmd: Commands): Buffer | undefined {
+  switch (cmd.command) {
+    case "ping": {
+      const command_byte = Buffer.from(new Uint8Array([0x01]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "set_location": {
+      const command_byte = Buffer.from(new Uint8Array([0x10]).buffer);
+      const x_bytes = Buffer.from(new Int32Array([cmd.x]).buffer);
+      const y_bytes = Buffer.from(new Int32Array([cmd.y]).buffer);
+      const degree_bytes = Buffer.from(new Int32Array([cmd.degree * 100]).buffer);
+      return Buffer.concat([command_byte, x_bytes, y_bytes, degree_bytes]);
+    }
+    case "arm_stop": {
+      const command_byte = Buffer.from(new Uint8Array([0x50]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "right_arm_move": {
+      const command_byte = Buffer.from(new Uint8Array([0x51]).buffer);
+      let box_number = Buffer.alloc(0);
+      switch (cmd.box) {
+        case "A": {
+          box_number = Buffer.from(new Uint8Array([0]).buffer);
+        }
+        case "B": {
+          box_number = Buffer.from(new Uint8Array([1]).buffer);
+        }
+        case "C": {
+          box_number = Buffer.from(new Uint8Array([2]).buffer);
+        }
+        case "D": {
+          box_number = Buffer.from(new Uint8Array([3]).buffer);
+        }
+        case "E": {
+          box_number = Buffer.from(new Uint8Array([4]).buffer);
+        }
+      }
+      return Buffer.concat([command_byte, box_number]);
+    }
+    case "left_arm_move": {
+      const command_byte = Buffer.from(new Uint8Array([0x52]).buffer);
+      let box_number = Buffer.alloc(0);
+      switch (cmd.box) {
+        case "A": {
+          box_number = Buffer.from(new Uint8Array([0]).buffer);
+        }
+        case "B": {
+          box_number = Buffer.from(new Uint8Array([1]).buffer);
+        }
+        case "C": {
+          box_number = Buffer.from(new Uint8Array([2]).buffer);
+        }
+        case "D": {
+          box_number = Buffer.from(new Uint8Array([3]).buffer);
+        }
+        case "E": {
+          box_number = Buffer.from(new Uint8Array([4]).buffer);
+        }
+      }
+      return Buffer.concat([command_byte, box_number]);
+    }
+    case "right_arm_fold_upper": {
+      const command_byte = Buffer.from(new Uint8Array([0x5A]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "left_arm_fold_upper": {
+      const command_byte = Buffer.from(new Uint8Array([0x5B]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "right_arm_fold_lower": {
+      const command_byte = Buffer.from(new Uint8Array([0x5C]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "left_arm_fold_lower": {
+      const command_byte = Buffer.from(new Uint8Array([0x5D]).buffer);
+      return Buffer.concat([command_byte]);
+    }
+    case "arm_suction_on_off": {
+      const command_byte = Buffer.from(new Uint8Array([0x60]).buffer);
+      let is_on_byte = Buffer.alloc(0);
+      if (cmd.is_on) {
+        is_on_byte = Buffer.from(new Uint8Array([1]).buffer);
+      } else {
+        is_on_byte = Buffer.from(new Uint8Array([0]).buffer);
+      }
+      return Buffer.concat([command_byte, is_on_byte]);
+    }
+  }
+
   return undefined;
 }
